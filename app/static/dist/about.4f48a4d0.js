02c6e04b68360e12e688241910ae4f16606d2d90
Pierre GIRAUD <pierre.giraud@dalibo.com>
2021-02-22 12:09:43 +0100

    Update pev2 to 0.18.0

--- app/static/dist/about.4f48a4d0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["about"],{6:function(n,o,i){"use strict";i.r(o);i(9),i(11),i(12),i(8),i(13)},8:function(n,o,i){}},[[6,"runtime","vendors~about~index~plan"]]]);
