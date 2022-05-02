575451037ade9c9ae37816082b5ccde7d27b4083
kevin.malot <kevin.malot@doctolib.com>
2022-05-02 12:00:55 +0200

    doc uri variable

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'aSup3rS33kret')
 DEBUG = os.environ.get('DEBUG')
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG_TB_PROFILER_ENABLED = True
-# GOOGLE_ANALYTICS = "UA-XXXXXX-XX"
+
+DOCUMENTATION_URI = "https://mydocumentation.com/visualizer"
