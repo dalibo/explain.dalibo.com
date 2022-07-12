07a072ca4af854806d19d5f4f08aa3cf0e70ad2e
Pierre GIRAUD <pierre.giraud@dalibo.com>
2022-07-12 17:19:46 +0200

    Upgrade PEV2 to 1.2.0
    Requires an migration to Vite and Vue3
    Plan are now always stored in the Database.

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,37 +1,40 @@
-import './common.js';
-import axios from 'axios';
-import Vue from 'vue';
-import moment from 'moment';
-import _ from 'lodash';
-import $ from 'jquery';
-import VueTimeago from 'vue-timeago';
-import Notifications from 'vue-notification';
+import "./common.js";
+import axios from "axios";
+import { createApp } from "vue/dist/vue.esm-bundler";
+import moment from "moment";
+import timeago from "vue-timeago3";
+import _ from "lodash";
+import $ from "jquery";
+import "vite/modulepreload-polyfill";
 
-Vue.use(VueTimeago, {locale: 'en'});
-Vue.use(Notifications);
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-new Vue({
-  el: "#app",
-  data: function() {
+// Add all icons to the library
+library.add(fas);
+
+const app = createApp({
+  data: function () {
     return {
       samples: [
-        ['Example 1 (JSON)', 'plan_1.json', 'plan_1.sql'],
-        ['Example 1 (plain text)', 'plan_1.txt', 'plan_1.sql'],
-        ['Example 2', 'plan_2.json', 'plan_2.sql'],
-        ['Example 3', 'plan_3.json', 'plan_3.sql'],
-        ['Example 4', 'plan_4.json'],
-        ['Example 5', 'plan_5.json', 'plan_5.sql'],
-        ['With subplan', 'plan_6.txt'],
-        ['With CTE', 'plan_7.txt'],
-        ['Very large plan', 'plan_8.json'],
-        ['With trigger', 'plan_trigger.json', 'plan_trigger.sql'],
-        ['With trigger (plain text)', 'plan_trigger.txt', 'plan_trigger_2.sql'],
-        ['Parallel (verbose)', 'plan_parallel.json'],
-        ['Parallel (4 workers)', 'plan_parallel2.txt', 'plan_parallel2.sql']
+        ["Example 1 (JSON)", "plan_1.json", "plan_1.sql"],
+        ["Example 1 (plain text)", "plan_1.txt", "plan_1.sql"],
+        ["Example 2", "plan_2.json", "plan_2.sql"],
+        ["Example 3", "plan_3.json", "plan_3.sql"],
+        ["Example 4", "plan_4.json"],
+        ["Example 5", "plan_5.json", "plan_5.sql"],
+        ["With subplan", "plan_6.txt"],
+        ["With CTE", "plan_7.txt"],
+        ["Very large plan", "plan_8.json"],
+        ["With trigger", "plan_trigger.json", "plan_trigger.sql"],
+        ["With trigger (plain text)", "plan_trigger.txt", "plan_trigger_2.sql"],
+        ["Parallel (verbose)", "plan_parallel.json"],
+        ["Parallel (4 workers)", "plan_parallel2.txt", "plan_parallel2.sql"],
       ],
-      titleInput: '',
-      planInput: '',
-      queryInput: '',
+      titleInput: "",
+      planInput: "",
+      queryInput: "",
       draggingPlan: false,
       draggingQuery: false,
       plans: [],
@@ -40,45 +43,42 @@ new Vue({
     };
   },
   watch: {
-    planToDelete: function(newPlan, oldPlan) {
+    planToDelete: function (newPlan, oldPlan) {
       this.deleteFromServer = false;
-    }
+    },
   },
   mounted() {
-    const textAreas = document.getElementsByTagName('textarea');
+    const textAreas = document.getElementsByTagName("textarea");
     this.loadPlans();
   },
   methods: {
-
     checkForm(event) {
       var form = event.target;
       event.preventDefault();
-      this.titleInput = this.titleInput || 'Plan created on ' + moment().format("MMMM Do YYYY, h:mm a");;
+      this.titleInput =
+        this.titleInput ||
+        "Plan created on " + moment().format("MMMM Do YYYY, h:mm a");
       var createdOn = new Date();
-      var id = 'plan_' + createdOn.getTime().toString();
-      localStorage.setItem(id,
-        JSON.stringify({
-          id: id,
-          title: this.titleInput,
-          plan: this.planInput,
-          query: this.queryInput,
-          createdOn: createdOn
-        })
-      );
-      window.location.href = '/plan#' + id;
+      var form = event.target;
+      this.share(form.action, {
+        title: this.titleInput,
+        plan: this.planInput,
+        query: this.queryInput,
+        createdOn: createdOn,
+      });
     },
 
     loadSample(sample) {
       this.titleInput = sample[0];
-      axios.get(staticUrl + 'samples/' + sample[1]).then((response) => {
+      axios.get(staticUrl + "samples/" + sample[1]).then((response) => {
         this.planInput = response.request.responseText;
       });
       if (sample[2]) {
-        axios.get(staticUrl + 'samples/' + sample[2]).then((response) => {
+        axios.get(staticUrl + "samples/" + sample[2]).then((response) => {
           this.queryInput = response.request.responseText;
         });
       } else {
-        this.queryInput = '';
+        this.queryInput = "";
       }
     },
 
@@ -98,8 +98,8 @@ new Vue({
         if (reader.result instanceof ArrayBuffer) {
           return;
         }
-        input.value = reader.result || '';
-        input.dispatchEvent(new Event('input'));
+        input.value = reader.result || "";
+        input.dispatchEvent(new Event("input"));
       };
       reader.readAsText(file);
     },
@@ -107,21 +107,22 @@ new Vue({
     loadPlans() {
       var plans = [];
       for (var i in localStorage) {
-        if (_.startsWith(i, 'plan_')) {
+        if (_.startsWith(i, "plan_")) {
           plans.push(JSON.parse(localStorage[i]));
         }
       }
 
-      this.plans = _.chain(plans).sortBy('createdOn').reverse().value();
+      this.plans = _.chain(plans).sortBy("createdOn").reverse().value();
     },
 
     getPlanUrl(plan) {
-      return plan.shareId ? '/' + plan.shareId : '#' + plan.id;
+      return plan.shareId ? "/" + plan.shareId : "#" + plan.id;
     },
 
     deletePlan(plan) {
       if (this.deleteFromServer && plan.shareId) {
-        axios.get('/plan/' + plan.shareId + '/' + plan.deleteKey)
+        axios
+          .get("/plan/" + plan.shareId + "/" + plan.deleteKey)
           .then(this.onPlanDelete.bind(this, plan));
       } else {
         this.onPlanDelete(plan);
@@ -129,9 +130,39 @@ new Vue({
     },
 
     onPlanDelete(plan) {
-      localStorage.removeItem(plan.id ? plan.id : 'plan_' + plan.shareId);
+      localStorage.removeItem(plan.id ? plan.id : "plan_" + plan.shareId);
       this.loadPlans();
-      $('#deletePlanModal').modal('hide');
-    }
-  }
+      $("#deletePlanModal").modal("hide");
+    },
+
+    share(url, plan) {
+      axios
+        .post(url, {
+          title: plan.title,
+          plan: plan.plan,
+          query: plan.query,
+        })
+        .then((response) => {
+          localStorage.removeItem(plan.id);
+          var data = response.data;
+          const id = "plan_" + data.id;
+          localStorage.setItem(
+            id,
+            JSON.stringify({
+              id: id,
+              shareId: data.id,
+              title: plan.title,
+              createdOn: plan.createdOn,
+              deleteKey: data.deleteKey,
+            })
+          );
+
+          // redirect to page with plan from server
+          window.location.href = "/plan/" + data.id;
+        });
+    },
+  },
 });
+app.use(timeago);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
--- app/static/js/plan.js
+++ app/static/js/plan.js
@@ -1,99 +1,31 @@
 import './common.js';
-import Vue from 'vue';
-import pev2 from 'pev2';
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import { Plan } from "pev2"
 import axios from 'axios';
 import bootstrap from 'bootstrap';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import VueTour from 'vue-tour';
-import 'vue-tour/dist/vue-tour.css';
-Vue.use(VueTour);
+import "pev2/dist/style.css";
 
-const tourVersion = '1.16';
+import { library  } from "@fortawesome/fontawesome-svg-core"
+import { fas  } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon  } from "@fortawesome/vue-fontawesome"
 
-new Vue({
+// Add all icons to the library
+library.add(fas)
+
+const app = createApp({
   el: "#app",
   data: function() {
     return {
       plan: null,
-      tourOptions: {
-        labels: {
-          buttonStop: 'Understood, thanks'
-        }
-      },
-      tourSteps: [
-        {
-          target: '#shareButton',
-          content: `The plan is not saved on the server until you click this button.<br>
-          Once done, you'll get a permanent link you can share with others.`
-        }
-      ],
-      tourCallbacks: {
-        onFinish () {
-          localStorage.setItem('tour', tourVersion);
-        }
-      }
     };
   },
   components: {
-    pev2: pev2
-  },
-  computed: {
-    planInLocalStorage () {
-      // finds the plan in local storage
-      for (var i in localStorage) {
-        if (_.startsWith(i, 'plan_')) {
-          const plan = JSON.parse(localStorage[i]);
-          if (plan.shareId == this.plan.shareId) {
-            return localStorage.getItem(i);
-          }
-        }
-      }
-      return false;
-    },
+    pev2: Plan
   },
   created () {
-    if (window.plan !== undefined) {
-      // Load from script tag
-      this.plan = plan;
-    } else {
-      this.id = location.hash.replace('#', '');
-      // Load from localStorage
-      this.plan = JSON.parse(localStorage.getItem(this.id));
-      if (this.plan) {
-        this.plan.sql = this.plan.query;
-        document.title = this.plan.title + ' ' + document.title;
-        location.hash = '';
-      } else {
-        // No hash in URL redirect to home page
-        window.location.href = "/plan_error";
-      }
-    }
-    const finishedTour = localStorage.getItem('tour');
-    if (!finishedTour || finishedTour !== tourVersion) {
-      const tour = this.$tours['tour'];
-      tour && tour.start();
-    }
-  },
-
-  methods: {
-    share(event) {
-      var form = event.target;
-      //event.preventDefault();
-      axios.post(form.action, {
-        title: this.plan.title,
-        plan: this.plan.plan,
-        query: this.plan.query
-      }).then((response) => {
-        var data = response.data;
-        var planFromStorage = JSON.parse(localStorage.getItem(this.id));
-        planFromStorage.shareId = data.id;
-        planFromStorage.deleteKey = data.deleteKey;
-        localStorage.setItem(this.id, JSON.stringify(planFromStorage));
-
-        // redirect to page with plan from server
-        window.location.href = "/plan/" + data.id + location.hash;
-      });
-    }
+    this.plan = plan;
   }
 });
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
