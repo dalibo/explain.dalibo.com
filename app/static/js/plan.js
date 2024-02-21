0277f539e3c41f41a0db40bf9d9f1bcf4ea81e3a
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-02-21 11:22:28 +0100

    Update PEV2 to 1.9.1
    Along the way changes were required since Bootstrap is also updated to
    5.3.2.

--- app/static/js/plan.js
+++ app/static/js/plan.js
@@ -1,31 +1,30 @@
-import './common.js';
-import { createApp } from 'vue/dist/vue.esm-bundler';
-import { Plan } from "pev2"
-import axios from 'axios';
-import bootstrap from 'bootstrap';
+import "./common.js";
+import { createApp } from "vue/dist/vue.esm-bundler";
+import { Plan } from "pev2";
+import axios from "axios";
 
 import "pev2/dist/style.css";
 
-import { library  } from "@fortawesome/fontawesome-svg-core"
-import { fas  } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon  } from "@fortawesome/vue-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // Add all icons to the library
-library.add(fas)
+library.add(fas);
 
 const app = createApp({
   el: "#app",
-  data: function() {
+  data: function () {
     return {
       plan: null,
     };
   },
   components: {
-    pev2: Plan
+    pev2: Plan,
   },
-  created () {
+  created() {
     this.plan = plan;
-  }
+  },
 });
 app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
