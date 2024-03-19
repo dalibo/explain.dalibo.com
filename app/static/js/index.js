ca5cb0ab298b826a22f748b899073a74c44c7d62
Pierre GIRAUD <pierre.giraud@dalibo.com>
2024-03-19 09:59:57 +0100

    Only load the required icons

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -9,11 +9,10 @@ import { Modal } from "bootstrap";
 import { onMounted, ref, watch } from "vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faLink, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-// Add all icons to the library
-library.add(fas);
+library.add(faLink, faTrashAlt);
 
 let confirmModal;
 let deleteModal;
