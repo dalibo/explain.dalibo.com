d3d637dcd71f82f9015ccc35bdd1d25a23d99b2e
Pierre GIRAUD <pierre.giraud@dalibo.com>
2026-01-20 14:30:56 +0100

    feat(ui): add a dark/light theme switcher

---
+++ app/static/js/themeSwitcher.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="nav-item ms-2">
+    <a
+      class="nav-link"
+      href="#"
+      @click="toggleTheme"
+      :title="theme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode'"
+    >
+      <font-awesome-icon :icon="theme === 'dark' ? 'moon' : 'sun'" />
+    </a>
+  </li>
+</template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faMoon, faSun);
+
+import { useColorMode } from "@vueuse/core";
+
+const { store: theme } = useColorMode({
+  attribute: "data-bs-theme",
+  storageKey: "theme",
+  initialValue: "auto",
+});
+
+function toggleTheme() {
+  theme.value = theme.value === "dark" ? "light" : "dark";
+}
+</script>
